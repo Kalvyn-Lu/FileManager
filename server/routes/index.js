62588e0aac599449826598015f51aa401be044fb
Brent Echols <brent.echols@webfilings.com>
2015-11-22 13:42:06 -0700

    api working better

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,15 +4,10 @@ import path from 'path';
 let router = express.Router();
 
 export default (app) => {
+    info(app);
     app.use('/js', express.static('../client/dist/js'));
     app.use('/css', express.static('../client/dist/css'));
-    app.use('/api', router);
     app.use('/', function(req, res) {
-        console.log('found path: ', path.join(__dirname, '../../client/dist', 'index.html'));
         res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
     });
-
-    console.log('updating');
-
-    // info(app);
 };
