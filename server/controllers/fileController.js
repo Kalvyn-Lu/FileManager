80ccd99fc06e2d5b1202445d57f9cd2b3cf25c49
Brent Echols <brent.echols@webfilings.com>
2015-11-22 15:42:55 -0700

    filled in file controller with more stubs

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -3,16 +3,16 @@ function getFiles() {
 	return Promise.resolve({msg: 'we got some files!'});
 }
 
-function getFile(id) {
-	return Promise.resolve({msg: 'we got a file!'});
+function getFile({id}) {
+	return Promise.resolve({msg: `we got a file! ${id}`});
 }
 
-function writeFile(id, data) {
-
+function writeFile({id, data}) {
+	return Promise.resolve({msg: `we are updating a file! ${id}`});
 }
 
-function deleteFile(id) {
-
+function deleteFile({id}) {
+	return Promise.resolve({msg: `we are deleting a file! ${id}`});
 }
 
 export default {
