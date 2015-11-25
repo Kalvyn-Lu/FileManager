798cfdbd98b2b107aaf8f396e24a06f77aa0e434
kalvyn-lu <kalvynlu@gmail.com>
2015-11-24 20:07:20 -0700

    untested writeFile

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -3,27 +3,43 @@ import immutable from 'immutable';
 import recordController from './recordController'
 
 let files = immutable.Map();
+
+const recordSize = 1024;
+
 class File {
-	constructor() {
+	constructor(id) {
 		this.name = '';
-		this.id = uuid();
+		this.id = id !== undefined ? id:uuid();
 		this.records = []; // list of record ids
+		this.size = 0;
 	}
 }
 
+
 function getFiles() {
+
 	return Promise.resolve({msg: 'we got some files!'});
 }
 
 function getFile({id}) {
+	
 	return Promise.resolve({msg: `we got a file! ${id}`});
 }
 
 function writeFile({id, data}) {
-	File create = new File();
+ 	let create = new File(id);
 	create.name = data.name;
-	create.id = id;
-	Math.floor()
+	let strData = data.content;
+	for(let i = 0; i < strData.length; i+= recordSize){
+		let cSlice;
+		if(strData.length < (i + recordSize)){
+			cSlice = strData.slice(i,strData.length);
+		}else {
+			cSlice = strData.slice(i,recordSize);
+		}
+		create.records.append(recordController.writeRecord({id:null,data:cSlice}));
+	}
+	files = files.set(id,create);
 	return Promise.resolve({msg: `we are updating a file! ${id}`});
 }
 
