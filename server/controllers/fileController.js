210b591f10d48a1d72c3e3d11fb05a27ca34b265
Brent Echols <brent.echols@webfilings.com>
2015-11-24 18:45:39 -0700

    record controller has documentation sort of

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -3,32 +3,32 @@ import immutable from 'immutable';
 
 let files = immutable.Map();
 class File {
-	constructor() {
-		this.name = '';
-		this.id = uuid();
-		this.records = []; // list of record ids
-	}
+    constructor() {
+        this.name = '';
+        this.id = uuid();
+        this.records = []; // list of record ids
+    }
 }
 
 function getFiles() {
-	return Promise.resolve({msg: 'we got some files!'});
+    return Promise.resolve({msg: 'we got some files!'});
 }
 
 function getFile({id}) {
-	return Promise.resolve({msg: `we got a file! ${id}`});
+    return Promise.resolve({msg: `we got a file! ${id}`});
 }
 
 function writeFile({id, data}) {
-	return Promise.resolve({msg: `we are updating a file! ${id}`});
+    return Promise.resolve({msg: `we are updating a file! ${id}`});
 }
 
 function deleteFile({id}) {
-	return Promise.resolve({msg: `we are deleting a file! ${id}`});
+    return Promise.resolve({msg: `we are deleting a file! ${id}`});
 }
 
 export default {
-	getFiles,
-	getFile,
-	writeFile,
-	deleteFile
+    getFiles,
+    getFile,
+    writeFile,
+    deleteFile
 };
--- server/controllers/recordController.js
+++ server/controllers/recordController.js
@@ -1,23 +1,61 @@
+import uuid from 'tiny-uuid';
+import immutable from 'immutable';
 
+const recordSize = 1024;
+const cacheSize = 100;
+
+class RecordTable {
+    constructor(json) {
+        this.nextId = 0;
+        this.records = immutable.Map();
+    }
+
+    requestId() {
+        let index = this.nextId;
+
+        // Keep scrolling through the record table until we find a hole, or reach the end
+        while (this.records.get(index) !== undefined) {
+            index++;
+        }
+
+        return index;
+    }
+
+    fillRecordBuffer(record) {
+        
+    }
+}
+
+class Record {
+    constructor(id) {
+        this.id = id;
+        this.buffer = null;
+    }
+}
+
+// Returns a list of all of the record objects, without any buffer information
 function getRecords() {
-	return Promise.resolve({msg: 'we got some Records!'});
+    return Promise.resolve({msg: 'we got some Records!'});
 }
 
+// Returns a full record object
 function getRecord({id}) {
-	return Promise.resolve({msg: `we got a Record! ${id}`});
+    return Promise.resolve({msg: `we got a Record! ${id}`});
 }
 
+// Writes a record to the table, leaving 'id' null creates a new record
 function writeRecord({id, data}) {
-	return Promise.resolve({msg: `we are updating a Record! ${id}`});
+    return Promise.resolve({msg: `we are updating a Record! ${id}`});
 }
 
+// Deletes the specified record from the table
 function deleteRecord({id}) {
-	return Promise.resolve({msg: `we are deleting a Record! ${id}`});
+    return Promise.resolve({msg: `we are deleting a Record! ${id}`});
 }
 
 export default {
-	getRecords,
-	getRecord,
-	writeRecord,
-	deleteRecord
+    getRecords,
+    getRecord,
+    writeRecord,
+    deleteRecord
 };
