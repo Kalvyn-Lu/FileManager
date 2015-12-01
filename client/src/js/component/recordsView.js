d232fd627b508b637d74a76679b300402ff00663
Brent Echols <brent.echols@webfilings.com>
2015-11-30 20:01:34 -0700

    basic styling for records view

--- client/src/js/component/recordsView.js
+++ client/src/js/component/recordsView.js
@@ -5,6 +5,7 @@ import component from 'component';
 import records from '../store/records';
 
 const recordPath = ['@@recordsView/records'];
+const recordSize = 1024;
 
 export default component({
     displayName: 'recordsView',
@@ -16,11 +17,25 @@ export default component({
     },
 
     render() {
-        console.log(this.getViewState().toJS());
         let recordList = this.getViewState(recordPath, emptyList);
 
-        return div({},
-            recordList.map(x => div({}, JSON.stringify(x.toJS())))
+        return div({className: 'fm-content'},
+            div({className: 'fm-record-item'},
+                div({className: 'fm-record-item-id'}, 'ID'),
+                div({className: 'fm-record-size'}, 'Size'),
+                div({className: 'fm-record-size-bar'})
+            ),
+            recordList.map(x => {
+                let recordPercent = Math.floor(x.get('size') / recordSize * 100);
+
+                return div({className: 'fm-record-item'},
+                    div({className: 'fm-record-item-id'}, x.get('id')),
+                    div({className: 'fm-record-size'}, x.get('size')),
+                    div({className: 'fm-record-size-bar'},
+                        div({className: 'fm-record-size-bar-fill', style: {width: `${recordPercent}%`}})
+                    )
+                );
+            })
         );
     }
 });
