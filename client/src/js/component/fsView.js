3e4b038fa3900377724bc4306bea6410d718ac9d
kalvyn-lu <kalvynlu@gmail.com>
2015-12-04 19:19:30 -0700

    files base

--- client/src/js/component/fsView.js
+++ client/src/js/component/fsView.js
@@ -1,13 +1,29 @@
 import React from 'react';
 const {div} = React.DOM;
+import {emptyList, routeNames} from 'constants';
 import component from 'component';
+import files from '../store/files';
+
+import {Link} from './router-jsx';
+
+const filesPath = ['@@filesView/files'];
 
 export default component({
-    displayName: 'Shell',
+    displayName: 'filesView',
+
+    mixins: [files.store.connectTo([], filesPath)],
+
+    componentDidMount() {
+        files.actions.fetchFiles();
+    },
 
     render() {
-        return div({},
-            'fsview'
+        let filesList = this.getViewState(filesPath, emptyList);
+
+        return div({className: 'content'},'fsView',
+            filesList.map(x => {
+                
+            })
         );
     }
 });
