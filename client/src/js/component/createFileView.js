28e685e0198b40846cc1c99c8269a66febd69b9c
Daniel <dpatposs@msn.com>
2015-12-05 15:04:00 -0700

    Inner workings for create file inprogress

--- client/src/js/component/createFileView.js
+++ client/src/js/component/createFileView.js
@@ -18,11 +18,15 @@ export default component({
 
     componentDidMount() {
         files.actions.fetchFiles();
+        console.log('Hit');
     },
 
     render() {
         // let filesList = this.getViewState(createFilePath, emptyList).toList();
 
-        return div();
-    },
+        return div({className: 'fm-content'},
+            div({className: 'fm-create-file-list'},
+              div({className: 'fm-create-file-item-header'},
+                div({className: 'fm-create-file-item-name'}, 'File Name'))));
+    }
 });
