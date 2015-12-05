fdc51a4c050289b83868ea5158816af34c832344
Daniel <dpatposs@msn.com>
2015-12-05 15:47:14 -0700

    createFile page almost finished

--- client/src/js/component/createFileView.js
+++ client/src/js/component/createFileView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-const {div} = React.DOM;
+const {div,textarea,button} = React.DOM;
 import {emptyList, routeNames} from 'constants';
 import component from 'component';
 import files from '../store/files';
@@ -18,15 +18,23 @@ export default component({
 
     componentDidMount() {
         files.actions.fetchFiles();
-        console.log('Hit');
     },
 
     render() {
         // let filesList = this.getViewState(createFilePath, emptyList).toList();
+        let editText = this.getViewState('Contents of Created File');
 
         return div({className: 'fm-content'},
-            div({className: 'fm-create-file-list'},
-              div({className: 'fm-create-file-item-header'},
-                div({className: 'fm-create-file-item-name'}, 'File Name'))));
+                div({className: 'fm-create-file-item-name'}, 'File Name'),
+                  textarea({value: '', ref: 'textarea', className: 'fm-create-file-name-text-content', onChange: this.onTextNameChange}),
+                div({className: 'fm-create-file-item-contents'}, 'Contents of File'),
+                  textarea({value: editText, ref:'textarea', className: 'fm-create-file-content-text-content', onChange: this.onTextContentsChange}),
+                button({value: 'Create File', ref: 'button', className: 'fm-create-file-button'}),
+                );
+    },
+
+    onTextChange(e) {
+      let text = e.target.value;
+      this.setViewState('Contents of Created File');
     }
 });
