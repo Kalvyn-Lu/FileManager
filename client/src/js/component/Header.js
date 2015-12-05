90f84f6fa204c5018f1dc25f01443b842a7181ea
Brent Echols <brent.echols@webfilings.com>
2015-12-05 13:46:52 -0700

    added header links

--- client/src/js/component/Header.js
+++ client/src/js/component/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 const {div, span} = React.DOM;
 import component from 'component';
+import {Link} from './router-jsx';
+import {routeNames} from 'constants';
 
 export default component({
     displayName: 'Header',
 
     render() {
         return div({className: 'fm-header'},
-            span({className: 'fm-header-title'}, 'File Manager')
+            span({className: 'fm-header-title'}, 'File Manager'),
+            Link({className: 'fm-header-link', to: routeNames.recordList}, 'Records'),
+            Link({className: 'fm-header-link', to: routeNames.fileList}, 'Files'),
         );
     }
 });
